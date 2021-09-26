8e3967eff20f417f079bffd28b6492d739c7463c
Palmitas <andrea.palma0121@gmail.com>
2021-09-26 01:30:13 -0300

    Create Marco.java

---
+++ src/main/java/com/aventurasdemarcoyluis/Marco.java
@@ -0,0 +1,7 @@
+package com.aventurasdemarcoyluis;
+
+public class Marco extends Player {
+    public Marco(int level, int attack, int defense, int healPoints, int fightPoints) {
+        super(level, attack, defense, healPoints, fightPoints);
+    }
+}
