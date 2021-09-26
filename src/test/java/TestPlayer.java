b9d7362e7855b325a6737d90c71e6d2ef7544b23
Palmitas <andrea.palma0121@gmail.com>
2021-09-26 04:23:55 -0300

    Se importan todas las variables

--- src/test/java/TestPlayer.java
+++ src/test/java/TestPlayer.java
@@ -1,5 +1,11 @@
 import com.aventurasdemarcoyluis.Marco;
-import com.aventurasdemarcoyluis.Player;
+import com.aventurasdemarcoyluis.Luis;
+import com.aventurasdemarcoyluis.Goomba;
+import com.aventurasdemarcoyluis.Spiny;
+import com.aventurasdemarcoyluis.Boo;
+import com.aventurasdemarcoyluis.HoneySyrup;
+import com.aventurasdemarcoyluis.Star;
+import com.aventurasdemarcoyluis.RedMushroom;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,19 +13,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestPlayer {
-    private Player p1;
+    private Marco marco;
+    private Luis luis;
+    private Goomba goomba;
+    private Spiny spiny;
+    private Boo boo;
+    private HoneySyrup honeySyrup;
+    private Star star;
+    private RedMushroom redMushroom;
 
     @BeforeEach
     public void setUp() {
-        p1 = new Marco(8, 9, 11, 40, 23);
+        marco = new Marco(8, 9, 11, 40, 23);
+        luis = new Luis(10, 11, 6, 51, 6);
+
     }
 
     @Test
-    public void constructorTest() {
-        assertEquals(p1.getMaxHP(),40);
-        assertEquals(p1.getHP(),40);
-        p1.setHP(-50);
-        assertEquals(p1.getMaxHP(),40);
-        assertEquals(p1.getHP(),0);
+    public void testConstructor() {
+        // Todos los Player y Enemigos vienen de Entities
+        // de modo que solo se testea una vez.
+        // HP
+        assertEquals(marco.getMaxHP(),40);
+        assertEquals(marco.getHP(),40);
+        marco.setHP(-50);
+        assertEquals(marco.getMaxHP(),40);
+        assertEquals(marco.getHP(),0);
+        marco.setHP(60);
+        assertEquals(marco.getMaxHP(),40);
+        assertEquals(marco.getHP(),40);
+        // FP
+        assertEquals(luis.getMaxFP(),6);
+        assertEquals(luis.getFP(),6);
+        marco.setFP(2);
+        assertEquals(luis.getFP(),2);
+        marco.setFP(-30);
+        assertEquals(luis.getMaxHP(),6);
+        assertEquals(luis.getHP(),0);
+        // Para
     }
 }
