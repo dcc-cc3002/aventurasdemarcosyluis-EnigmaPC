ed8b11bf189a10c4ba02141aa30af12a071f6f5e
Palmitas <andrea.palma0121@gmail.com>
2021-09-26 01:30:50 -0300

    Create Luis.java

---
+++ src/main/java/com/aventurasdemarcoyluis/Luis.java
@@ -0,0 +1,7 @@
+package com.aventurasdemarcoyluis;
+
+public class Luis extends Player {
+    public Luis(int level, int attack, int defense, int healPoints, int fightPoints) {
+        super(level, attack, defense, healPoints, fightPoints);
+    }
+}
