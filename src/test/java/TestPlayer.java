0a6a07b272ac47805d9a27a3f0d3021ecd6391b4
Palmitas <andrea.palma0121@gmail.com>
2021-09-25 23:10:53 -0300

    Update Test's
    Se crea TestPlayer para provar lo realizado.

---
+++ src/test/java/TestPlayer.java
@@ -0,0 +1,24 @@
+import com.aventurasdemarcoyluis.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestPlayer {
+    private Player p1;
+
+    @BeforeEach
+    public void setUp() {
+        p1 = new Player(8, 9, 11, 40, 23);
+    }
+
+    @Test
+    public void constructorTest() {
+        assertEquals(p1.getMaxHP(),40);
+        assertEquals(p1.getHP(),40);
+        p1.setHP(-50);
+        assertEquals(p1.getMaxHP(),40);
+        assertEquals(p1.getHP(),0);
+    }
+}
