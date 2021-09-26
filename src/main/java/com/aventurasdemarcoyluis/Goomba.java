a82b1195cd46f252575131d4b5b2259ce419e3d1
Palmitas <andrea.palma0121@gmail.com>
2021-09-25 23:11:45 -0300

    Update Goomba.java
    Se hace Goomba subclase de Enemy

--- src/main/java/com/aventurasdemarcoyluis/Goomba.java
+++ src/main/java/com/aventurasdemarcoyluis/Goomba.java
@@ -1,5 +1,8 @@
 package com.aventurasdemarcoyluis;
 
-public class Goomba {
+public class Goomba extends Enemy {
 
+    public Goomba(int level, int attack, int defense, int healPoints, int fightPoints) {
+        super(level, attack, defense, healPoints, fightPoints);
+    }
 }
