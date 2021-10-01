324f1cfc04e7b6f075cd0fb40a2f3af776b1282d
Palmitas <andrea.palma0121@gmail.com>
2021-09-30 21:53:11 -0300

    Update

--- src/test/java/com/aventurasdemarcoyluis/enemies/GoombaTest.java
+++ src/test/java/com/aventurasdemarcoyluis/enemies/GoombaTest.java
@@ -2,28 +2,24 @@ package com.aventurasdemarcoyluis.enemies;
 
 import com.aventurasdemarcoyluis.abstract_classes.BaseTest;
 import com.aventurasdemarcoyluis.players.Luis;
+import com.aventurasdemarcoyluis.players.Marco;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BooTest extends BaseTest {
+class GoombaTest extends BaseTest {
 
     @BeforeEach
     void setUp() {
-        boo = new Boo(20,23,17,60);
+        goomba = new Goomba(20,23,17,60);
 
         // Jugadores
+        marco = new Marco(25,27,30,50,19);
         luis = new Luis(13,6,15,20,7);
     }
 
     @Test
-    void attackTest() {
-        // Boo solo ataca a luis
-        // El daño que hace boo a luis es 23
-        boo.attack(luis);
-        assertEquals(luis.getHP(),0);
-        boo.attack(luis);
-        assertEquals(luis.getHP(),0);
+    void attack() {
     }
 }
