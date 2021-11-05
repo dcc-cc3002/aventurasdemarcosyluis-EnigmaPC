e215e16d9bd13b3948e4ef25d40757ec8062cbaf
Palmitas <andrea.palma0121@gmail.com>
2021-11-05 18:13:02 -0300

    Update GameController

--- src/main/java/com/aventurasdemarcoyluis/controller/GameController.java
+++ src/main/java/com/aventurasdemarcoyluis/controller/GameController.java
@@ -4,7 +4,6 @@ import com.aventurasdemarcoyluis.model.enemies.Boo;
 import com.aventurasdemarcoyluis.model.enemies.Goomba;
 import com.aventurasdemarcoyluis.model.enemies.Spiny;
 import com.aventurasdemarcoyluis.model.interfaces.IEntities;
-import com.aventurasdemarcoyluis.model.interfaces.IPlayer;
 import com.aventurasdemarcoyluis.model.players.Luis;
 import com.aventurasdemarcoyluis.model.players.Marco;
 
@@ -12,49 +11,84 @@ import java.util.*;
 
 
 public class GameController {
-    private final ArrayList<IEntities> listOfCharacter;
+    private final ArrayList<IEntities> listOfCharacters;
     private int nivelBatalla;
+    private int turn;
+    private IEntities turnEntity;
+    private IEntities actualEntity;
+    private int round;
 
 
     public GameController() {
-        listOfCharacter = new ArrayList<>();
+        listOfCharacters = new ArrayList<>();
         nivelBatalla = 1;
+        turn = 1;
+        round = 1;
     }
 
     public Luis addLuis(int level, int attack, int defense, int maxHealPoints, int maxFightPoints) {
         Luis luis = new Luis(level, attack, defense, maxHealPoints, maxFightPoints);
-        listOfCharacter.add(luis);
+        listOfCharacters.add(luis);
 
         return luis;
     }
 
     public Marco addMarco(int level, int attack, int defense, int maxHealPoints, int maxFightPoints) {
         Marco marco = new Marco(level, attack, defense, maxHealPoints, maxFightPoints);
-        listOfCharacter.add(marco);
+        listOfCharacters.add(marco);
 
         return marco;
     }
 
     public Boo addBoo(int level, int attack, int defense, int maxHealPoints) {
         Boo boo = new Boo(level, attack, defense, maxHealPoints);
-        listOfCharacter.add(boo);
+        listOfCharacters.add(boo);
 
         return boo;
     }
 
     public Goomba addGoomba(int level, int attack, int defense, int maxHealPoints) {
         Goomba goomba = new Goomba(level, attack, defense, maxHealPoints);
-        listOfCharacter.add(goomba);
+        listOfCharacters.add(goomba);
 
         return goomba;
     }
 
     public Spiny addSpiny(int level, int attack, int defense, int maxHealPoints) {
         Spiny spiny = new Spiny(level, attack, defense, maxHealPoints);
-        listOfCharacter.add(spiny);
+        listOfCharacters.add(spiny);
 
         return spiny;
     }
 
+    public List<IEntities> getListOfCharacters() {
+        return listOfCharacters;
+    }
+
+    public IEntities getTurnEntity() {
+        turnEntity = listOfCharacters.get((turn - 1) % listOfCharacters.size());
+        return turnEntity;
+    }
+
+    public void finishTurn() {
+        if (turn % listOfCharacters.size() == 0) {
+            round++;
+        }
+        setTurn(turn + 1);
+    }
+
+    public void setTurn(int turn) {
+        this.turn = turn;
+        actualEntity = getTurnEntity();
+    }
+
+    public int getRound() {
+        return round;
+    }
+
+    public int personalDice() {
+        int dice = getTurnEntity().roll();
+        return dice;
+    }
 
 }
